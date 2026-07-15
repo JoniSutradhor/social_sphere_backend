de8942a1b37c7fd6509d413246c8b95b1449c8ff
J.K. Sutradhor <jksutradhor@gmail.com>
2026-07-15 11:58:33 +0600

    Final update

--- src/validators/auth.validators.ts
+++ src/validators/auth.validators.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 const passwordSchema = z
   .string()
-  .min(10, "Password must be at least 10 characters")
+  .min(8, "Password must be at least 8 characters")
   .regex(/[a-z]/, "Password must contain a lowercase letter")
   .regex(/[A-Z]/, "Password must contain an uppercase letter")
   .regex(/[0-9]/, "Password must contain a digit")
